dd9be77d2094348be2cbd53b75e2ce742872f54a
Mukhammadali Khayotov <31897554+MuhammadAliHayotov@users.noreply.github.com>
2022-10-16 17:38:24 +0500

    Last changes

---
+++ Bankey/Bankey/AccountSummaryHeaderView.swift
@@ -0,0 +1,14 @@
+//
+//  AccountSummaryHeaderView.swift
+//  Bankey
+//
+//  Created by Mukhammadali Khayotov on 16/10/22.
+//
+
+import UIKit
+
+class AccountSummaryHeaderView: UIView {
+    
+    @IBOutlet var contentView: UIView!
+    
+}
